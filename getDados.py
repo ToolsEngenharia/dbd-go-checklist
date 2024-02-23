759d54f5ed08b09acf42cb8e9aea9219145e7a65
Leonardo Silva <ldss1@outlook.com>
2024-02-23 10:58:29 -0300

    Update Google Sheets URL in getDadosSheet function

--- getDados.py
+++ getDados.py
@@ -5,7 +5,7 @@ import streamlit as st
 ## Função para obter os dados - GOOGLE SHEETS
 @st.cache_data()
 def getDadosSheet():
-	url = "https://script.google.com/macros/s/AKfycbyDdODiPH0uyVx42JRe59MAsOPYL-4DPLTsIUIchmhoZfJB8nYKli3voiTHK-1xtUpxpQ/exec"
+	url = "https://script.google.com/macros/s/AKfycbxBemeIG-iFCz_FHeCwy0WTlyPTqKrEiAJLsvrM6FSomivdJZc4PmMrkD3i7mhZGD6t-A/exec"
 	response = req.get(url)
 	dados = response.json()
 	return dados['dados']
